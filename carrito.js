0cfd2803ab5c67726c619c4421c7b2eb4c84bb3e
FerchumanX86 <122957990+FerchumanX86@users.noreply.github.com>
2023-03-08 07:42:27 -0300

    dark

--- carrito.js
+++ carrito.js
@@ -1,8 +1,29 @@
 let carrito = []; // Array para almacenar los productos del carrito
 let total = 0; // Variable para almacenar el total de la compra
 
+//Funcion Dark (minuto 1:30)
+const botonFondo =document.getElementById("botonFondo");
+
+botonFondo.addEventListener("click",()=>{
+  document.body.classList.toggle("dark");
+  if(document.body.classList.contains("dark")){
+    localStorage.setItem("modo", "dark")
+  } else{
+    localStorage.setItem("modo", "ligth");
+  }
+})
+
+//Recuperamos el modo del localStorage
+const modo =localStorage.getItem("modo");
+if(modo === "dark"){
+  document.body.classList.add("dark");
+}else {
+  document.body.classList.remove("dark");
+}
+
 // Función para agregar un producto al carrito
 function agregarProducto(nombre, precio) {
+  
   // Crear un objeto con los datos del producto
   const producto = {
     nombre: nombre,
@@ -15,7 +36,7 @@ function agregarProducto(nombre, precio) {
   // Actualizar la lista de productos del carrito
   const listaCarrito = document.getElementById("lista-carrito");
   const elemento = document.createElement("li");
-  elemento.innerText = `${nombre} - $${precio}`;
+  elemento.innerText = `${nombre} - ${precio}`;
   listaCarrito.appendChild(elemento);
 
   // Actualizar el total del carrito
@@ -27,22 +48,43 @@ function agregarProducto(nombre, precio) {
 
 function finalizarCompra() {
     // Obtener el valor del elemento con el ID "total-carrito"
-    var total = document.getElementById("total-carrito").innerText;
+    let total = document.getElementById("total-carrito").innerText;
   
-    // Obtener la fecha actual y formatearla como una cadena legible por humanos
-    var fecha = new Date().toLocaleDateString();
+    // Obtener la fecha actual y formatearla como una cadena legible 
+    let fecha = new Date().toLocaleDateString();
   
     // Almacenar el valor del total y la fecha en el almacenamiento local del navegador
     localStorage.setItem("compra", "Total: " + total + " - Fecha: " + fecha);
   
     // Mostrar una alerta con el total y la fecha de compra
-    alert("Gracias por su compra. Su pedido ha sido procesado.\n\n" + "Total: " + total + "\nFecha: " + fecha);
+    // alert("Gracias por su compra. Su pedido ha sido procesado.\n\n" + "Total: " + total + "\nFecha: " + fecha);
 
-        swal("Gracias por su compra. Su pedido ha sido procesado.\n\n" + "Total: " + total + "\nFecha: " + fecha);
-        //🤔 queria cambiar el alert por un swal pero no funciona, si alguno me puede dar una mano con eso 
-
-    // Recargar la página
-    location.reload();
+    // Modifico el aler para usar SweetAlert
+    swal({
+      title: "¡Gracias por tu compra!",
+      text: "Tu pedido ha sido procesado correctamente.",
+      icon: "success",
+      buttons: {
+        confirm: {
+          text: "Ok",
+          value: true,
+          visible: true,
+          className: "btn btn-primary",
+          closeModal: true,
+          onclick: function() {
+            location.reload();
+          }
+        }
+      },
+      content: {
+        element: "div",
+        attributes: {
+          innerHTML: "Total: " + total + "<br>Fecha: " + fecha
+        }
+      }
+    });
+    
   }
 
 
+ 
