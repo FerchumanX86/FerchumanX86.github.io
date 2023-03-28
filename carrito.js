ec61846e16167211f661253408e99dd52bbc6dd9
FerchumanX86 <122957990+FerchumanX86@users.noreply.github.com>
2023-03-28 09:27:14 -0300

    CoffeHaus final

--- carrito.js
+++ carrito.js
@@ -45,6 +45,66 @@ function agregarProducto(nombre, precio) {
   totalCarrito.innerText = total.toFixed(2);
 }
 
+function vaciarCarrito() {
+  // Vaciar el array del carrito
+  carrito = [];
+
+  // Vaciar la lista de productos del carrito
+  const listaCarrito = document.getElementById("lista-carrito");
+  listaCarrito.innerHTML = '';
+
+  // Restablecer el total del carrito a 0
+  total = 0;
+  const totalCarrito = document.getElementById("total-carrito");
+  totalCarrito.innerText = total.toFixed(2);
+}
+
+const accessKey = "xuu00EhTOzmUj4fDG_EhvZS-qD8On5vsRZMpohw7DTY"; // Reemplaza esto con tu clave de acceso de Unsplash
+
+
+function obtenerImagenes() {
+  const productos = ["cup of tea", "Cup of coffee", "smoothie"];
+  const idsImagenes = ["imagen-te", "imagen-cafe", "imagen-licuado"];
+
+  productos.forEach((producto, index) => {
+    fetch(
+      `https://api.unsplash.com/search/photos?query=${producto}&client_id=${accessKey}&per_page=1&orientation=landscape`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.results && data.results.length > 0) {
+          const imagenUrl = data.results[0].urls.small;
+          const imagenProducto = document.getElementById(idsImagenes[index]);
+          imagenProducto.style.backgroundImage = `url(${imagenUrl})`;
+        }
+      })
+      .catch((error) => {
+        console.error("Error al obtener imágenes:", error);
+      });
+  });
+}
+
+
+// Llama a la función obtenerImagenes al cargar la página
+window.addEventListener("load", obtenerImagenes);
+
+
+function mostrarCompras() {
+  let compras = JSON.parse(localStorage.getItem("compras")) || [];
+
+  if (compras.length === 0) {
+      alert("No hay compras realizadas.");
+      return;
+  }
+
+  let comprasTexto = compras
+      .map((compra, index) => {
+          return `Compra ${index + 1}:\nTotal: ${compra.total}\nFecha: ${compra.fecha}`;
+      })
+      .join("\n\n");
+
+  alert("Compras realizadas:\n\n" + comprasTexto);
+}
 
 function finalizarCompra() {
     // Obtener el valor del elemento con el ID "total-carrito"
@@ -52,6 +112,16 @@ function finalizarCompra() {
   
     // Obtener la fecha actual y formatearla como una cadena legible 
     let fecha = new Date().toLocaleDateString();
+
+    
+  
+      // Almacenar el valor del total y la fecha en el almacenamiento local del navegador como un array
+      let compras = JSON.parse(localStorage.getItem("compras")) || [];
+      compras.push({ total: total, fecha: fecha });
+      localStorage.setItem("compras", JSON.stringify(compras));
+  
+      // Resto del código de la función finalizarCompra()...
+  
   
     // Almacenar el valor del total y la fecha en el almacenamiento local del navegador
     localStorage.setItem("compra", "Total: " + total + " - Fecha: " + fecha);
